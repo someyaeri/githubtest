70396434dc602764415e699a86c953af14f12905
someyaeri <g2130217@edu.cc.ocha.ac.jp>
2024-04-24 14:15:01 +0900

    update
    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
-
 function draw() {
-  ellipse(50, 50, 80, 80);
+  background(255);
+  for (var y = 0; y <= 1000; y = y + 500) {
+    for (var x = 0; x <= 1000; x = x + 500) {
+      noFill();
+      stroke(255, 147, 206);
+      rotateX(frameCount * 0.01);
+      rotateY(frameCount * 0.01);
+      box(200, 200, 200);
+    }
+  }
 }
